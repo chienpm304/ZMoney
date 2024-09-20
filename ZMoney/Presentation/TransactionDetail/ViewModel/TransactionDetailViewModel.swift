cddcd4307c7352ddccf7eebfc3fadd1ef87dcde0
Chien Pham <minhchien.pham@shopee.com>
2024-09-20 13:18:17 +0700

    [Fix] Fixed update selected category after modify categories list

--- ZMoney/Presentation/TransactionDetail/ViewModel/TransactionDetailViewModel.swift
+++ ZMoney/Presentation/TransactionDetail/ViewModel/TransactionDetailViewModel.swift
@@ -120,8 +120,9 @@ class TransactionDetailViewModel: ObservableObject, AlertProvidable {
     private func setupDataModel(categories: [DMCategory]) {
         self.categories = categories
 
-        if isNewTransaction, transaction.category.isPlaceholder,
-            let defaultCategory = filteredCategories.first {
+        if let updatedCategory = filteredCategories.first(where: { $0.id == transaction.category.id }) {
+            transaction.category = updatedCategory
+        } else if isNewTransaction, transaction.category.isPlaceholder, let defaultCategory = filteredCategories.first {
             transaction.category = defaultCategory
             transaction.category.isPlaceholder = false
         }
